5a67c1a8a08088ba0994028f21d022470e5f5a5c
Brett Snaidero <brettsnaidero@hotmail.com>
2023-07-12 16:45:07 +1000

    chore: change package name

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const APP_META_TITLE = 'P.S. Salts';
+export const APP_META_TITLE = 'Next.js Vendure storefront starter kit';
 export const APP_META_DESCRIPTION =
   'A headless commerce storefront starter kit built with Next & Vendure';
 export const DEMO_API_URL = 'TODO';
